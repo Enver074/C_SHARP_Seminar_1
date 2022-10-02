6afa508249533d18eb69e4e010a8bee772feedc4
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-10-02 23:58:58 +0300

    Решение второй задачи

--- Program.cs
+++ Program.cs
@@ -4,14 +4,39 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите первое число");
-double Num1 = double.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите второе число");
-double Num2 = double.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите первое число");
+// double Num1 = double.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число");
+// double Num2 = double.Parse(Console.ReadLine()!);
 
-if(Num1 > Num2){
-    Console.WriteLine("max = " + Num1);}
-if(Num1 == Num2){
-    Console.WriteLine("max = " + Num1);}
-else{
-    Console.WriteLine("max = " + Num2);}
+// if(Num1 > Num2){
+//     Console.WriteLine("max = " + Num1);}
+// if(Num1 == Num2){
+//     Console.WriteLine("max = " + Num1);}
+// else{
+//     Console.WriteLine("max = " + Num2);}
+
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Введите первое число");
+// double Num1 = double.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число");
+// double Num2 = double.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите третье число");
+// double Num3 = double.Parse(Console.ReadLine()!);
+
+// double max;
+// max = Num1;
+// for (int i = 1; i < 3; i++){
+// if(Num2 > Num1)
+//     max = Num2; 
+
+// if(Num3 > Num2)
+//     max = Num3;
+// }
+// Console.WriteLine(max);
